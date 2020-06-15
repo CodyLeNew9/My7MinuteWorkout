330299e7345e15f8d969399f1cbf87af0c6dd250
Cody Newman <FortifiedITS@gmail.com>
2020-06-15 11:11:41 -0600

    Activity XML

--- app/src/main/java/com/example/my7minuteworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/my7minuteworkout/ExerciseActivity.kt
@@ -2,10 +2,25 @@ package com.example.my7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_exercise.*
 
 class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+
+        //Setups as an action bar
+        setSupportActionBar(toolbar_exercise_activity)
+        val actionBar = supportActionBar
+
+        // gives action bar back button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
+
+        // makes action bar back button work like screen back button
+        toolbar_exercise_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
